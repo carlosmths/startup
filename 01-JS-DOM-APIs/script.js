c374722ff65dc60f600f9e60be4bd9776e3603da
carlosmths <carlosmths@gmail.com>
2017-05-01 15:57:04 -0300

    Topic 1 Complete
    Added the repository finder functionality plus the table creator as
    required in the guide.

--- 01-JS-DOM-APIs/script.js
+++ 01-JS-DOM-APIs/script.js
@@ -1,11 +1,17 @@
 window.onload = function(){
 	const makeVisible = document.getElementById('jokeSection');
 	const btnLoader = document.getElementById('btnJoke');
+	const btnRepoData = document.getElementById('btnRepoData');
+	const btnCreateTable = document.getElementById('btnCreateTable');
 	makeVisible.className = "visible";
 	// btnLoader.onclick = jokeLoader;
 	btnLoader.onclick = getJoke;
+	btnRepoData.onclick = getRepositoryData;
+	btnCreateTable.onclick = matrixToTable;
 }
 
+//First jokeLoader function. Not used after makeAjaxRequest was made.
+/*---------------------------------------------------------------------------*/
 const jokeLoader = () => {
 	const xmlhttp = new XMLHttpRequest();
 	const jokePar = document.getElementById('jokeParagraph');
@@ -28,22 +34,24 @@ const jokeLoader = () => {
 	}
 	xmlhttp.open('GET', 'http://api.icndb.com/jokes/random', true);
 	xmlhttp.send();
-	
 }
+/*---------------------------------------------------------------------------*/
 
-//Configuration object that will be used on reusableAjaxRequest() function
-const ajaxConfig = {
+//Configuration object that will be used on reusableAjaxRequest() function.
+//Will be variable instead of constant, to be modified afterwards.
+var ajaxConfig = {
 	//Request URL
-	url:'http://api.icndb.com/jokes/random',
-	//Method (i.e. GET or POST)
-	method:'GET'
+	url:'',
+	//Method (i.e. GET or POST). Default is GET
+	method:'GET',
+	//Data to send
+	parameters:''
 }
 
 const makeAjaxRequest = config => {
 	const xmlhttp = new XMLHttpRequest();
 	const jokePar = document.getElementById('jokeParagraph');
 	const btnLoader = document.getElementById('btnJoke');
-	
 	const getData = new Promise((resolve, reject) => {
 		xmlhttp.onload = () => {
 			let status = xmlhttp.status;
@@ -58,29 +66,108 @@ const makeAjaxRequest = config => {
 		xmlhttp.onerror = () => {
 			reject('Request Error!');
 		}
+		if(config.method == 'GET'){
+			config.url += `?q=${config.parameters}`
+		}
+		else{
+			reject(`Can´t use method ${config.method} on this request`)
+		}
 		xmlhttp.open(config.method, config.url, true);
-		xmlhttp.send();
+		xmlhttp.send(config.parameters);
 	})
 	return getData;
 }
 
-const getAndPrint = (cfg) => {
+const getAndPrint = (cfg, elementToFill, lookFor) => {
 	makeAjaxRequest(cfg)
 	.then(result => {
 		const data = JSON.parse(result);
-		printData('jokeParagraph', data.value.joke);
+		if(lookFor == 'getJoke'){
+			printData(elementToFill, data.value.joke);
+			changeBtnText();
+		}
+		else if(lookFor == 'getRepo'){
+			removeChilds(elementToFill);
+			processList(elementToFill, data);
+		}
+		else{
+			printData(elementToFill, `Parameter: ${lookFor} not recognized`)
+		}
 	})
 	.catch(error => {
-		const section = document.getElementById('jokeSection');
-		section.style.color = 'red';
-		printData('jokeParagraph', error)
+		elementToFill.className += ' error';
+		printData(elementToFill, error)
 	});
 }
 
-const printData = (id,text) => {
-	document.getElementById(id).innerHTML = text
+const printData = (element,text) => {
+	element.innerHTML = text
+}
+
+const changeBtnText = () => {
+	document.getElementById('btnJoke').innerHTML = 'Load another joke!';
 }
 
 const getJoke = () => {
-	getAndPrint(ajaxConfig)
+	const jokeContainer = document.getElementById('jokeParagraph'); 
+	ajaxConfig.url = 'http://api.icndb.com/jokes/random';
+	getAndPrint(ajaxConfig, jokeContainer, 'getJoke');
 }
+
+const getRepositoryData = () => {
+	const dataContainer = document.getElementById('repoContainer');
+	ajaxConfig.url = 'https://api.github.com/search/repositories';
+	const search = document.getElementById('txtSearch').value;
+	ajaxConfig.parameters = `${search}`;
+	getAndPrint(ajaxConfig, dataContainer, 'getRepo');
+}
+
+const processList = (element, list) => {
+	const items = list.items;
+	const repoContainer = element;
+	items.forEach(makeList);
+}
+
+const makeList = (items, index) => {
+	console.log(`items[${index}] = ${items.full_name}`);
+	let li = document.createElement('li');
+	let textNode = document.createTextNode(`${items.full_name}`);
+	li.appendChild(textNode);
+	repoContainer.appendChild(li);
+}
+
+//Removes childs from given element to stop adding elements to the DOM. Must be invoked before appending childs in functions.
+const removeChilds = (element) => {
+	while(element.firstChild){
+		element.removeChild(element.firstChild);
+	}
+}
+
+/*---------------------------------------------------------------------------*/
+
+const matrixToTable = () => {
+	const tableSection = document.getElementById('tableSection');
+	removeChilds(tableSection);
+	const table = document.createElement('table');
+	table.className = 'table';
+	const tableBody = document.createElement('tbody');
+	tableBody.className = 'tbody';
+	matrix.forEach(function(rowData) {
+	    const row = document.createElement('tr');
+
+	    rowData.forEach(function(cellData) {
+			const cell = document.createElement('td');
+			cell.appendChild(document.createTextNode(cellData));
+			row.appendChild(cell);
+	    });
+
+	    tableBody.appendChild(row);
+  	});
+
+  table.appendChild(tableBody);
+  const section = document.getElementById('tableSection')
+  section.appendChild(table);
+}
+
+const matrix = [['Row1-Cell1', 'Row1-Cell2', 'Row1-Cell3'], 
+ ['Row2-Cell1', 'Row2-Cell2', 'Row2-Cell3'], ['Row3-Cell1', 'Row3-Cell2', 'Row3-Cell3']];
